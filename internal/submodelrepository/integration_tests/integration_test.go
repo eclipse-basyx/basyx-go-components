19d741d90a51503ff07cc6b2d6c5355d0079dc36
FriedJannik <jannik.fried@iese.fraunhofer.de>
2025-11-04 10:45:47 +0100

    Fixes linting issues

--- internal/submodelrepository/integration_tests/integration_test.go
+++ internal/submodelrepository/integration_tests/integration_test.go
@@ -33,7 +33,9 @@ func loadTestConfig(filename string) ([]TestConfig, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
+	defer func() {
+		_ = file.Close()
+	}()
 
 	var configs []TestConfig
 	decoder := json.NewDecoder(file)
@@ -85,7 +87,7 @@ func makeRequest(config TestConfig) (string, error) {
 		return "", err
 	}
 
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 
 	if resp.StatusCode != config.ExpectedStatus {
 		fmt.Printf("Response status code: %d\n", resp.StatusCode)
