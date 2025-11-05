e9b4d4e3778316863bcbf04bd799c8163a80e607
FriedJannik <jannik.fried@iese.fraunhofer.de>
2025-11-05 16:27:10 +0100

    Removes unused code

--- internal/common/queries/AdministrationSubquery.go
+++ internal/common/queries/AdministrationSubquery.go
@@ -71,7 +71,6 @@ type AdministrationRow struct {
 //	mainQuery := dialect.From("submodel").As("s").
 //	    Select(..., goqu.L("?", adminSubquery).As("administration"))
 func GetAdministrationSubquery(dialect goqu.DialectWrapper, joinConditionColumn string) *goqu.SelectDataset {
-	// administrativeInformationEmbeddedDataSpecificationReferenceSubquery, administrativeInformationEmbeddedDataSpecificationReferenceReferredSubquery, administrativeInformationIEC61360Subquery := GetEmbeddedDataSpecificationSubqueries(dialect, "administrative_information_embedded_data_specification", "administrative_information_id", joinConditionColumn)
 
 	// Build the jsonb object for administration creator references
 	creatorObj := goqu.Func("jsonb_build_object",
