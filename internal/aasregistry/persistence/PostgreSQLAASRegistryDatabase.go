508060fcb50296e98a8ba3e119dd5ef7c972be7b
Martin187187 <martin-stemmer@t-online.de>
2025-11-07 16:07:22 +0100

    fixed too large default limit

--- internal/aasregistry/persistence/PostgreSQLAASRegistryDatabase.go
+++ internal/aasregistry/persistence/PostgreSQLAASRegistryDatabase.go
@@ -426,7 +426,7 @@ func (p *PostgreSQLAASRegistryDatabase) ListAssetAdministrationShellDescriptors(
 ) ([]model.AssetAdministrationShellDescriptor, string, error) {
 
 	if limit <= 0 {
-		limit = 100000000
+		limit = 1000000
 	}
 	peekLimit := int(limit) + 1
 
