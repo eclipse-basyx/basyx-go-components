100dc9ee96eccc553d3c9485c348afd5090e0aa9
FriedJannik <jannik.fried@basyx.dev>
2026-02-03 12:04:55 +0100

    Code Review

--- internal/submodelrepository/integration_tests/it_dellog.sh
+++ internal/submodelrepository/integration_tests/it_dellog.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -r ./internal/submodelrepository/integration_tests/logs
-go test -v ./internal/submodelrepository/integration_tests    
+go test -v ./internal/submodelrepository/integration_tests
