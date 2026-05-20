3fd5526565eab3f14cf05fef8bc553f4118155bc
FriedJannik <jannik.fried@basyx.dev>
2026-05-20 11:20:32 +0200

    missing NL at EOF - fix annoying bug

--- internal/aasregistry/security_tests/aasregistry_security_integration_test.go
+++ internal/aasregistry/security_tests/aasregistry_security_integration_test.go
@@ -49,6 +49,7 @@ func TestIntegration(t *testing.T) {
 }
 
 func TestMain(m *testing.M) {
+	_ = os.Setenv("COMPOSE_PROJECT_NAME", "aasregistry-security-tests")
 	os.Exit(testenv.RunComposeTestMain(m, testenv.ComposeTestMainOptions{
 		ComposeFile: "docker_compose/docker_compose.yml",
 		HealthURL:   "http://localhost:6004/health",
