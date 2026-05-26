bea906989f19054ab4b78e731b50ef42eb64ec4c
Martin Stemmer <52048213+Martin187187@users.noreply.github.com>
2026-05-26 16:31:04 +0200

    fix 1.0.1 patch file

--- database/patches/1_0_1.sql
+++ database/patches/1_0_1.sql
@@ -25,3 +25,13 @@ ALTER TABLE IF EXISTS aas_identifier
 
 ALTER TABLE IF EXISTS aas_identifier
   ADD COLUMN IF NOT EXISTS db_updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW();
+
+UPDATE basyxsystem
+SET schema_version = 'v1.0.1',
+    state = 'clean'
+WHERE identifier = (
+  SELECT identifier
+  FROM basyxsystem
+  ORDER BY identifier ASC
+  LIMIT 1
+);
