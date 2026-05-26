69540e2ab17e88cd0338218ba4fe672976cf5d83
Martin Stemmer <52048213+Martin187187@users.noreply.github.com>
2026-05-26 16:23:06 +0200

    fix

--- database/patches/1_0_2.sql
+++ database/patches/1_0_2.sql
@@ -21,3 +21,13 @@ CREATE INDEX IF NOT EXISTS ix_submodel_descriptor_semantic_id_refpayload_refid
   ON submodel_descriptor_semantic_id_reference_payload(reference_id);
 CREATE INDEX IF NOT EXISTS ix_specasset_external_subject_id_refpayload_refid
   ON specific_asset_id_external_subject_id_reference_payload(reference_id);
+
+UPDATE basyxsystem
+SET schema_version = 'v1.0.2',
+    state = 'clean'
+WHERE identifier = (
+  SELECT identifier
+  FROM basyxsystem
+  ORDER BY identifier ASC
+  LIMIT 1
+);
