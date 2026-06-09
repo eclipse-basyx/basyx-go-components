d6fbd249c3e7f6872f456248cd754cfc8a735448
Aaron Zielstorff <aaron.zielstorff@iese.fraunhofer.de>
2026-06-09 10:28:04 +0200

    Fixes failing tests

--- internal/common/security/abac_engine_objects.go
+++ internal/common/security/abac_engine_objects.go
@@ -266,21 +266,12 @@ type RouteWithFilter struct {
 }
 
 func mapDescriptorValueToRoute(descriptorValue grammar.DescriptorValue, basePath string) []RouteWithFilter {
-	routes := mapScopedIdentifierValueToRoute(
+	return mapScopedIdentifierValueToRoute(
 		descriptorValue.Scope,
 		descriptorValue.ID,
 		basePath,
 		descriptorRouteMappings,
 	)
-
-	if descriptorValue.Scope == "$aasdesc" || descriptorValue.Scope == "$smdesc" {
-		routes = append(routes,
-			RouteWithFilter{route: joinBasePath(basePath, "/bulk/status/*")},
-			RouteWithFilter{route: joinBasePath(basePath, "/bulk/result/*")},
-		)
-	}
-
-	return routes
 }
 
 func mapIdentifiableValueToRoute(identifiableValue grammar.IdentifiableValue, basePath string) []RouteWithFilter {
